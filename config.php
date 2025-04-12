b76e34059af0380dfd8ec37eac3e7bb0abe6e910
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2025-04-12 18:41:41 +1000

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "database-1.caubbk7qzzzx.us-east-1.rds.amazonaws.com"; // or your DB host
+$dbname = "database1";
+$username = "admin"; // your DB username
+$password = "admin1234"; // your DB password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
